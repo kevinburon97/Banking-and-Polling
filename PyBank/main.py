d6d18cdfa5fd0d3ad2edc263a02f2adc4eb773e9
unknown <kevinburon97@gmail.com>
2020-06-27 00:37:04 -0400

    six commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@ import os
 import csv 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 csvpath = os.path.join("Resources","budget_data.csv")
-
+outputpath = os.path.join("Analysis", "Analysis.csv")
 with open(csvpath) as budgetdata:
     csvreader = csv.reader(budgetdata, delimiter=",")
    
@@ -13,16 +13,28 @@ with open(csvpath) as budgetdata:
     prevvalue = 0
     totalrev = 0
     monthlyrev = []
+    
     for row in csvreader: 
-        rowcount.append(row[1])  #add values to list rowcount
-        monthlyvalue=int(row[1])
-        totalprofit = totalprofit + monthlyvalue # net amount of profit/losses
+         #add values to list rowcount
+        rowcount.append(row[1]) 
+        monthlyvalue = int(row[1])
+        # net amount of profit/losses
+        totalprofit = totalprofit + monthlyvalue 
     for x in range(1,len(rowcount)):
+        #Add to the list monthly rev the monthly change for each row
         monthlyrev.append(int(rowcount[x])-int(rowcount[x-1]))                      
-
-
+    
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    
+    with open(outputpath) as output:
+        csvwriter = csv.writer(output)
+        budgetdata.write("Financial Analysis")
+print("Financial Analysis")
+print( "---------------------------------")
 print("Total Months: " + str(len(rowcount)))
 print("Total is $" + str(totalprofit))
 print("Average Change: $"+str(round(sum(monthlyrev)/(len(rowcount)-1),2)))    
 print("Greatest increase in PRofits: $"+ str(max(monthlyrev)))      
 print("Greatest Decrease in Profits: $"+ str(min(monthlyrev)))
+
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,14 +5,16 @@ csvpath = os.path.join("Resources/election_data.csv")
 
 with open(csvpath) as electiondata: 
     csvreader = csv.reader(electiondata,delimiter=",")
-
+    #Skip header
     next(electiondata)
+    
     totalrows = []
     Khan = []
     Li = []
     Correy = []
     OTooley = []
-    for row in csvreader:
+    #Go through each row to find which vote went to which candidate and add it to their respective list
+    for row in csvreader: 
         totalrows.append(row[0])
         if row[2]=="Khan":
             Khan.append(row[2])
@@ -23,9 +25,27 @@ with open(csvpath) as electiondata:
         else:
             OTooley.append(row[2])
     
-
-print(len(totalrows))
-print(format(len(Khan)/len(totalrows),..0%)
-print(len(Li)/len(totalrows))
-print(len(Correy)/len(totalrows))
-print(len(OTooley)/len(totalrows))
+    #The % of votes each candidate had  
+    pctgKhan = len(Khan)/len(totalrows)
+    pctgLi =  len(Li)/len(totalrows)
+    pctgCorrey =  len(Correy)/len(totalrows)
+    pctgOTooley =  len(OTooley)/len(totalrows)
+print("Election Results:")
+print( "---------------------------------")
+print("Total Votes:" + str(len(totalrows)))
+print( "---------------------------------")
+print("Candidates:")
+print("Khan: " + format(pctgKhan,".2%") + " total votes: ("+ str(len(Khan)) + ")")
+print("Li: " + format(pctgLi,".2%") + " total votes: ("+ str(len(Li)) + ")")
+print("Correy: " + format(pctgCorrey,".2%")+ " total votes: ("+ str(len(Correy)) + ")")
+print("O'Tooley: " + format(pctgOTooley,".2%") + " total votes: ("+ str(len(OTooley)) + ")")
+print( "---------------------------------")
+#Find which candidate had the most votes and keep the # of votes under winner
+winner = max(len(Khan),len(Li),len(Correy),len(OTooley))
+#Create a dictionary listing the votes of each candidate with their name as a value
+biglist = {len(Khan): "Khan", len(Li): "Li", len(Correy): "Correy", len(OTooley): "O'Tooley"}
+#Go through the dictionary and print the one that matches winner
+for x in biglist:
+    if winner == x:
+       print("And the winner is: " +biglist.get(x))    
+print("---------------------------------")
